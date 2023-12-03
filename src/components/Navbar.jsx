5ed0e851d03f5e0ce0de4df4cdccfa08ffa8d686
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-12-03 17:18:21 +0300

    search bar added with it's styles, and instantiated in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import React from "react";
 import { logo } from "./../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -18,6 +18,7 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 );
 
