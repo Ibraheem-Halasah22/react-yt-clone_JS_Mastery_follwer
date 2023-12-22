35e72ea9e39acf730e8eba11e7c6dce2ef607a6a
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-12-22 23:06:27 +0300

    sidebar implemented, and added to the feed page

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,28 @@
+import { Stack, Box, Typography } from "@mui/material";
 import React from "react";
+import Sidebar from "./../components/Sidebar";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copy Right 2023 YT Clone
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
